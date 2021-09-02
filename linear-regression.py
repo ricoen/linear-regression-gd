0a6743cea1840d31dfe3dda3eda19e3e21b50e71
ricoen <rico.enrayan@gmail.com>
2021-09-02 21:04:53 +0700

    Changes

--- linear-regression.py
+++ linear-regression.py
@@ -2,10 +2,12 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+
 # Relationship between power (kW) and Torque (Nm) with Motor Speed 2000 rpm
 # https://www.engineeringtoolbox.com/electrical-motors-hp-torque-rpm-d_1503.html
 x = np.array([1.1, 1.5, 2.2, 3.7, 5.6])
 y = np.array([5.3, 7.1, 10.7, 18, 27])
+
 
 def gradient_descent(epochs, alpha):
     m = 0
@@ -22,12 +24,14 @@ def gradient_descent(epochs, alpha):
         c = c - alpha * D_c
     
     return [m, c]
+
         
 def predict(x_train):
     [m, c] = gradient_descent(1000, 0.01)
     print(m, c)
     Y_predict = m*x_train + c
     return Y_predict
+
 
 y_prediction = predict(x)
 y_prediction = np.round(y_prediction, 2)
